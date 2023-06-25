0db2659007515406c3a70d555b1d0f9545fc242f
arkim0111 <aramingk95@gmail.com>
2023-06-26 01:02:05 +0900

    초기 셋팅

---
+++ src/test/java/com/example/showlib/ShowlibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.showlib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowlibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
